e2cf213b9f738ddc3abd7158397c6cf277576293
Sappakit <pipitwattana.s@gmail.com>
2025-02-02 21:08:20 +0700

    feat: add HourlyTemperature component

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -1,4 +1,5 @@
 import CurrentWeather from "@/components/current-weather";
+import HourlyTemperature from "@/components/hourly-temperature";
 import WeatherSkeleton from "@/components/loading-skeleton";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
@@ -113,18 +114,20 @@ const WeatherDashboard = () => {
 
       {/* CUrrent and Hourly weather */}
       <div className="grid gap-6">
-        <div>
+        <div className="flex flex-col lg:flex-row gap-4">
+          {/* Current weather */}
           <CurrentWeather
             data={weatherQuery.data}
             locationName={locationName}
           />
-          {/* current weather */}
-          {/* hourly tempature */}
+
+          {/* Hourly temperature */}
+          <HourlyTemperature data={forecastQuery.data} />
         </div>
 
         <div>
-          {/* details */}
-          {/* forecast */}
+          {/* Details */}
+          {/* Forecast */}
         </div>
       </div>
     </div>
